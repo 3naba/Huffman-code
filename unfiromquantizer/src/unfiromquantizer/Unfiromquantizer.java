32dee93f398a2f2a24a9fbb04c12a5fba8db8682
ahmed <ahmed@ahmed.com>
2015-06-07 18:08:16 -0700

    quantization

---
+++ unfiromquantizer/src/unfiromquantizer/Unfiromquantizer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package unfiromquantizer;
+
+/**
+ *
+ * @author ahmed
+ */
+public class Unfiromquantizer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
